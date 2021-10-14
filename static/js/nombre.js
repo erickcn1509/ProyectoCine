6a8cc85b61b6b13177c8a9a7e4df090b18fc5c8b
Erickson Leonel Castillo Navarro <ericksonc@uninorte.edu.co>
2021-10-13 20:26:04 -0500

    pagina de registro

---
+++ static/js/nombre.js
@@ -0,0 +1,20 @@
+//alert("hola mundo")
+
+function validar_formulario() {
+    let nombre = document.getElementById("name").value
+    let apellido = document.getElementById("ape").value
+    let esFemenino = document.getElementById("F").checked
+    let esMasculino = document.getElementById("M").checked
+    let identificacion = document-getElementById("number").checked
+    let fechaNaci = document.getElementById("date").checked
+    let celular = document.getElementById("celular").checked
+    let correo = document.getElementById("email").value
+    let pass = document.getElementById("passwor").value
+    let passwor = document.getElementById("passwordconfirm").value
+    let datos = document.getElementById("datos").checked
+
+
+    let sw = true
+
+    return sw
+}
