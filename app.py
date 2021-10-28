8cc5d9383575c554e04a6a546ed2d59f90692992
Erickson Leonel Castillo Navarro <ericksonc@uninorte.edu.co>
2021-10-27 23:26:51 -0500

    Registro con la db

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template,jsonify, redirect
-import db
+from flask import Flask, render_template,request, redirect, jsonify, send_file, session
+import db, io, werkzeug.security as ws
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'cine.db'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def funcion():
@@ -15,13 +22,55 @@ def funcion2():
 def funcion3():
     return render_template('cartelera.html')
 
-@app.route('/registro.html')
+@app.route('/registro.html', methods=['GET', 'POST'])
 def funcion4():
+    if request.method == 'POST':
+        details = request.form
+        nombre = details['nombre']
+        apellido = details['apellido']
+        sexo = details['sexo']
+        identificacion = details['identificacion']
+        fecha_naci = details['fecha_naci']
+        celular = details['celular']
+        correo = details['correo']
+        password = details['password']
+        cur = db.comexion_sql()
+        cursor = cur.cursor()
+        cursor.execute("INSERT INTO usuario (nombre, apellido, sexo, identificacion, fecha_naci, celular, correo, password) VALUES ('{}', '{}', '{}', '{}','{}', '{}', '{}', '{}')".format(nombre, apellido, sexo, identificacion, fecha_naci, celular, correo, password))
+        cur.commit()
+        cur.close()
+        return 'succes'
     return render_template('registro.html')
 
 @app.route('/index.html')
 def funcion5():
     return render_template('index.html')
+
+@app.route('/ingresar.html', methods=['GET', 'POST'])
+def funcion6(): 
+    data = []
+    if request.method == 'GET':
+        cur = db.comexion_sql()
+        cursor = cur.cursor()
+        getData = cursor.execute("SELECT correo, password FROM usuario")
+        data = getData.fetchall()
+        
+        print (data)
+        cur.commit()
+        cur.close()
+    else:
+        details2 = request.form
+        correo_login = details2['correo_login']
+        password_login = details2['password_login']
+        for i in data:
+            if i[0] == correo_login and i[1] == password_login:
+                return 'sesión iniciada'
+            else:
+                return render_template('index.html')
+        
+    return render_template('ingresar.html')
+
+
 
 @app.route('/<string:tabla>')
 def index(tabla):
@@ -41,3 +90,5 @@ def consulta_tabla(tabla):
 
     con.close()
     return datos
+
+
--- db.py
+++ db.py
@@ -1,5 +1,8 @@
 import sqlite3
 from sqlite3 import Error
+from flask import Flask
+
+
 
 def comexion_sql():
     try:
@@ -7,6 +10,33 @@ def comexion_sql():
         return con;
     except Error:
         print(Error) 
+ 
+
+""" def comexion_sql():
+    
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+
+    with app.app_context():
+        from . import routes  # Import routes
+        db.create_all()  # Create sql tables for our data models
+
+        return app """
 
 def cerrar(con):
     con.close()
+
+def insertar_usuario(nombre, apellido, sexo, identificacion, fecha_naci, celular, correo, password):
+    try:
+        con = comexion_sql()
+        cursor = con.cursor()
+        
+        strsql = "INSERT INTO usuario (nombre, apellido, sexo, identificacion, fecha_naci, celular, correo, password) VALUES ('{}', '{}', '{}', '{}','{}', '{}', '{}', '{}')".format(nombre, apellido, sexo, identificacion, fecha_naci, celular, correo, password)
+
+        cursor.execute(strsql)
+        con.commit()
+        con.cerrar()
+    except:
+        print("An error has occured")
